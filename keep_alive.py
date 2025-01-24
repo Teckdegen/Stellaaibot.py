7e4177a834fdebe53be00e1fb68ed0348f6c5924
Teckdegen <divinebassey.personal@gmail.com>
2025-01-25 00:38:24 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,12 @@
-from flask import, Flask
-from threading import Thread 
-
-app = flask(_name_)
-
-app.route('/')
- def index():
-    return"Alive"
-
+(for python) keep_alive.py:
+from flask import Flask,render_template
+from threading import Thread
+app = Flask(_name_)
+@app.route('/')
+def index():
+    return "Alive"
 def run():
-  app.run(host="0.0.0.0",port=8080)
-
-def keep_alive():
-    t=Thread(target=run)
+  app.run(host='0.0.0.0',port=8080)
+def keep_alive():  
+    t = Thread(target=run)
     t.start()
